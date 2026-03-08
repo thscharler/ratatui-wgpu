9f60488bc0ba8d77763ec46c2f4d112643616374
thscharler <thscharler@gmail.com>
2026-03-08 16:07:31 +0100

    fix: warnings before release.

--- examples/hello_pipeline.rs
+++ examples/hello_pipeline.rs
@@ -270,7 +270,7 @@ impl PostProcessor for Pipeline {
 
     fn process(
         &mut self,
-        margin_color: u32,
+        _margin_color: u32,
         encoder: &mut wgpu::CommandEncoder,
         queue: &wgpu::Queue,
         _text_view: &wgpu::TextureView,
